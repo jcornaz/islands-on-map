759e4ce82c097e385585316bf388c23330318ddd
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-01-01 19:58:12 +0100

    Upgrades dependencies (expect ktor)

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,13 +4,13 @@ object Version {
 
     const val KTOR = "1.0.1"
     const val SLF4J_SIMPLE = "1.8.0-beta2"
-    const val COROUTINES = "1.1.0-alpha"
+    const val COROUTINES = "1.1.0"
     const val PROTOBUF = "3.6.1"
 
-    const val NEO4J_DRIVER = "1.4.4"
+    const val NEO4J_DRIVER = "1.7.2"
     const val NEO4J = "3.5.1"
 
-    const val KLUENT = "1.44"
+    const val KLUENT = "1.45"
     const val SPEK = "2.0.0-rc.1"
     const val MOCKK = "1.8.13.kotlin13"
     const val MIOP = "0.3.0"
